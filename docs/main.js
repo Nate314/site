a2cc82bbbf0621c0c5ef5bc8797d484abdd85fca
Nathan Gawith <NathanGawith@gmail.com>
2019-06-18 22:19:48 -0500

    fixed 404 issue

--- docs/main.js
+++ docs/main.js
@@ -2332,7 +2332,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Natha\Documents\Development\Angular\site\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\Natha\Desktop\site\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- docs/styles.js
+++ docs/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Natha\Documents\Development\Angular\site\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\Natha\Desktop\site\src\styles.css */"./src/styles.css");
 
 
 /***/ })
